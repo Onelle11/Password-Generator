9e87a56ca4681a6d8479fe6571be98b561935da7
Onelle11 <ofullente@gmail.com>
2023-01-11 02:23:32 -0500

    added generatePassword function

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 // Assignment code here
+var lowerCase = "abcdef";
+var upperCase = "ABCDEF";
+var numbers = "12345";
+var specialCharacters = "!@#$";
+
+
+function generatePassword() {
+  var length = parseInt(window.prompt("Type the length of the password. 8 characters not more than 128"));
+  
+  if (length < 8) {
+    alert("the length entered is less than 8");
+    return "error";
+  }
+  if (length > 128) {
+    alert("the length entered is more than 128");
+    return "error";
+  }
+
+  var output  = "";
+  var passwordCharacters = lowerCase + upperCase + numbers + specialCharacters;
+  for (var i = 0; i < length; i++) {
+    output += passwordCharacters[Math.floor(passwordCharacters.length * Math.random())];
+  }
+    return output
+
+}
+
 
 
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -15,3 +43,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
